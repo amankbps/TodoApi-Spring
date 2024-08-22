d727572104db7f1f631b158bcb0342374de57d15
amankbps <amankbps@gmail.com>
2024-08-22 18:06:41 +0530

    basic controller stracutre

---
+++ src/test/java/org/example/todoapispringapplication/TodoApiSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.todoapispringapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApiSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
