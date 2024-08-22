1bb0613be8df6785869652d907fa723d81ad4794
amankbps <amankbps@gmail.com>
2024-08-22 18:47:03 +0530

    added  post api

--- src/main/java/org/example/todoapispringapplication/TodoController.java
+++ src/main/java/org/example/todoapispringapplication/TodoController.java
@@ -1,6 +1,9 @@
 package org.example.todoapispringapplication;
 
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -9,13 +12,26 @@ import java.util.List;
 @RestController
 public class TodoController {
 
-    private static List<Todo>todos;
+    private static List<Todo>todosList;
 
     public TodoController()
     {
-         todos = new ArrayList<>();
-         todos.add(new Todo(1,false,"Todo 1",1));
-        todos.add(new Todo(2,true,"Todo 2",2));
+        todosList = new ArrayList<>();
+        todosList.add(new Todo(1,false,"Todo 1",1));
+        todosList.add(new Todo(2,true,"Todo 2",2));
+    }
+
+    @GetMapping("/todos")
+    public List<Todo>getTodos()
+    {
+        return todosList;
+    }
+
+    @PostMapping("/todos")
+    public Todo addTodo(@RequestBody Todo todo)
+    {
+        todosList.add(todo);
+        return todo;
     }
 
 }
